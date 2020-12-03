5b0233176c6c6a5979cfc2ee5f849414afeae406
AlessandroMarcellettiUnicam1 <alessand.marcelletti@unicam.it>
2020-12-03 15:24:18 +0100

    update contract

--- test-network/scripts/query.sh
+++ test-network/scripts/query.sh
@@ -53,7 +53,7 @@ setGlobals 3
   local PEER0_ORG3=localhost:11051  # Agency
   local NAME_CC=quotation
   local CHANNEL="quotationchannel1"
-  local TX='{"Args":["getQuotation","quotation2"]}'
+  local TX='{"Args":["getQuotation","quotation1"]}'
 	set -x
   peer chaincode invoke -o $ORDERER --tls true --cafile $ORDERER_CA -C $CHANNEL -n $NAME_CC --peerAddresses $PEER0_ORG1 --tlsRootCertFiles $PEER0_ORG1_CA --peerAddresses $PEER0_ORG3 --tlsRootCertFiles $PEER0_ORG3_CA -c $TX
   res=$?
@@ -80,9 +80,26 @@ setGlobals 1
   cat log.txt
   verifyResult $res "invoke transaction 'getQuotations' has failed"
   successln "invoke transaction 'provideQuotation' success"
+}
 
+acceptQuotation(){
 
+# 1 = SupplierA, 2 = SupplierB, 3 = Agency
+setGlobals 3
 
+  local ORDERER=localhost:7050
+  local PEER0_ORG1=localhost:7051   # SupplierA
+  local PEER0_ORG3=localhost:11051  # Agency
+  local NAME_CC=quotation
+  local CHANNEL="quotationchannel1"
+  local TX='{"Args":["acceptQuotation","quotation1","accepted"]}'
+	set -x
+  peer chaincode invoke -o $ORDERER --tls true --cafile $ORDERER_CA -C $CHANNEL -n $NAME_CC --peerAddresses $PEER0_ORG1 --tlsRootCertFiles $PEER0_ORG1_CA --peerAddresses $PEER0_ORG3 --tlsRootCertFiles $PEER0_ORG3_CA -c $TX
+  res=$?
+  { set +x; } 2>/dev/null
+  cat log.txt
+  verifyResult $res "invoke transaction 'accept' has failed"
+  successln "invoke transaction 'accept' success"
 }
 
 
@@ -93,5 +110,6 @@ setGlobals 1
 #getQuotation
 #requestQuotation
 getQuotation
+#acceptQuotation
 
 exit 0
