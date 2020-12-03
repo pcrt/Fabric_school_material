ead7eb4e19fb3e675addd28821d67ce21af88de3
AlessandroMarcellettiUnicam1 <alessand.marcelletti@unicam.it>
2020-12-03 17:45:17 +0100

    updated getQuotation funciton in query file

--- test-network/scripts/query.sh
+++ test-network/scripts/query.sh
@@ -45,23 +45,31 @@ requestQuotation() {
 }
 
 getQuotation(){
-# 1 = SupplierA, 2 = SupplierB, 3 = Agency
-setGlobals 3
-
+  # 1 = SupplierA, 2 = SupplierB, 3 = Agency
+  IDENTITY=$1
+  CHANNEL=$2
+  setGlobals ${IDENTITY}
+  
   local ORDERER=localhost:7050
-  local PEER0_ORG1=localhost:7051   # SupplierA
   local PEER0_ORG3=localhost:11051  # Agency
   local NAME_CC=quotation
-  local CHANNEL="quotationchannel1"
+  local CHANNEL="quotationchannel"${CHANNEL}
   local TX='{"Args":["getQuotation","quotation1"]}'
 	set -x
-  peer chaincode invoke -o $ORDERER --tls true --cafile $ORDERER_CA -C $CHANNEL -n $NAME_CC --peerAddresses $PEER0_ORG1 --tlsRootCertFiles $PEER0_ORG1_CA --peerAddresses $PEER0_ORG3 --tlsRootCertFiles $PEER0_ORG3_CA -c $TX
-  res=$?
+  
+  case $1 in
+  1)
+  	local PEER0_ORG1=localhost:7051   # SupplierB 7051 per suppliera 9051 per B
+  	peer chaincode invoke -o $ORDERER --tls true --cafile $ORDERER_CA -C $CHANNEL -n $NAME_CC --peerAddresses $PEER0_ORG1 --tlsRootCertFiles $PEER0_ORG1_CA --peerAddresses $PEER0_ORG3 --tlsRootCertFiles $PEER0_ORG3_CA -c $TX;;
+  2)
+  	local PEER0_ORG2=localhost:9051   # SupplierB 7051 per suppliera 9051 per B
+  	peer chaincode invoke -o $ORDERER --tls true --cafile $ORDERER_CA -C $CHANNEL -n $NAME_CC --peerAddresses $PEER0_ORG2 --tlsRootCertFiles $PEER0_ORG2_CA --peerAddresses $PEER0_ORG3 --tlsRootCertFiles $PEER0_ORG3_CA -c $TX;;
+   esac
+   res=$?
   { set +x; } 2>/dev/null
   cat log.txt
   verifyResult $res "invoke transaction 'getQuotations' has failed"
   successln "invoke transaction 'getQuotation' success"
- 
 }
 
 provideQuotation(){
@@ -107,9 +115,11 @@ setGlobals 3
 
 #getQuotation
 #provideQuotation
-#getQuotation
+acceptQuotation
+getQuotation 1 1
+getQuotation 2 2
 #requestQuotation
 #getQuotation
-acceptQuotation
+
 
 exit 0
