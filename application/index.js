7f8b2083cdbc34ec92a34b00cc65e6a3e0f6151f
AlessandroMarcellettiUnicam1 <alessand.marcelletti@unicam.it>
2020-12-06 17:40:25 +0100

    Application folder created, it contains the node application for interacting with the quotation contract

---
+++ application/index.js
@@ -0,0 +1,128 @@
+const { Wallets, DefaultEventHandlerStrategies, Gateway } = require('fabric-network')
+const path = require('path')
+const fs = require('fs');
+const yaml = require('js-yaml');
+const fixtures = path.resolve(__dirname, '/../test-network');
+const { v4: uuidv4 } = require('uuid');
+
+
+const identity = 'Agency'
+//Paths in the code are relative to the folder of the fabric tutorial
+const walletPath = path.resolve(__dirname, `/home/winterschool20/Desktop/Winter_school/Fabric_school_material/test-network/${identity}/wallet`)
+
+let finalConnection = {}
+
+class test {
+
+    constructor() {}
+    //method for creating the identity related to an organization interacting with the network
+    static async createIdentity() {
+        try {
+           
+            //create new wallet
+            const wallet = await Wallets.newFileSystemWallet(walletPath)
+            //get identity from wallet
+            const userExists = await wallet.get(identity)
+            if (userExists) {
+                console.log(`WARN: An identity for the client user "${identity}" already exists in the wallet`)
+            }
+            //get credentials (certificate and private key) from the peer organization folder
+            const credPath = '/home/winterschool20/Desktop/Winter_school/Fabric_school_material/test-network/organizations/peerOrganizations/agency.quotation.com/users/User1@agency.quotation.com'
+            const certificate = fs.readFileSync(path.join(credPath, '/msp/signcerts/User1@agency.quotation.com-cert.pem')).toString();
+            const privateKey = fs.readFileSync(path.join(credPath, '/msp/keystore/priv_sk')).toString();
+            
+            const identityWallet = {
+                credentials: { certificate, privateKey },
+                mspId: 'AgencyMSP',
+                type: 'X.509'
+            }
+            console.log("first function ok")
+
+            await wallet.put(identity, identityWallet)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    
+    //Method for creating the connection with the blockchain
+    static async createConnection(){
+        try {
+           
+            const wallet = await Wallets.newFileSystemWallet(walletPath);
+            //get the connection profile file
+            const connOrg1Path = '/home/winterschool20/Desktop/Winter_school/Fabric_school_material/test-network/organizations/peerOrganizations/agency.quotation.com/connection-org3.yaml'
+            let connectionProfile = yaml.safeLoad(fs.readFileSync(connOrg1Path, 'utf8'));
+            //create connection option object
+            let connectionOptions = {
+                identity: identity,
+                wallet: wallet,
+                discovery: { enabled: true, asLocalhost: true },
+                eventHandlerOptions: {
+                // if strategy set to null, it will not wait for any commit events to be received from peers
+                    strategy: DefaultEventHandlerStrategies.MSPID_SCOPE_ALLFORTX
+                }
+            };
+    
+            
+           // save connection profile and option in global object 
+           // global.ConnectionProfiles[connectionID] = { connectionProfile, connectionOptions };
+           finalConnection = { connectionProfile, connectionOptions }
+            console.log("tutto ok seconda funzione")
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    
+    //method for sending a transaction 
+    static async submitT(){
+            try {
+            
+                //array containing the parameters to pass to the smart contract function
+                let transactionParams = ['quotation1']
+                //connect to the gateway to access the blockchain
+                const gateway = new Gateway();
+                
+                //const conn = global.ConnectionProfiles[connectionID];
+                const conn = finalConnection
+                await gateway.connect(conn.connectionProfile, conn.connectionOptions);
+                
+                //get and connect to the channel where the caincode is deployed
+                const network = await gateway.getNetwork('quotationchannel1');
+                //get the smart contract from the network
+                const contract = await network.getContract('quotation');
+                let resp = null
+            
+        //submit transaction
+                if(!transactionParams || transactionParams === '')
+                    resp = await contract.submitTransaction('getQuotation');
+                else
+                    resp = await contract.submitTransaction('getQuotation', transactionParams);
+                
+                gateway.disconnect();
+                console.log(resp.toString())
+        
+            } catch (err) {
+                console.log(err)
+            }
+    }
+
+}
+
+const main = async () => {
+    try {
+        await test.createIdentity()
+        await test.createConnection()
+        await test.submitT()
+    } catch (error) {
+        console.log(error)
+    }
+  
+
+  //  await test.createConnection()
+}
+main()
+
+
+
+
