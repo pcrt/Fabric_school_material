3f7d1ae9b62d651c69896db43258e2631ea72c98
EmanueleSc <scalaemanuele@gmail.com>
2020-12-09 12:06:42 +0100

    refactoring

--- application/index.js
+++ application/index.js
@@ -1,146 +1,164 @@
 const { Wallets, DefaultEventHandlerStrategies, Gateway } = require('fabric-network')
 const path = require('path')
-const fs = require('fs');
-const yaml = require('js-yaml');
-const fixtures = path.resolve(__dirname, '/../test-network');
-const { v4: uuidv4 } = require('uuid');
+const fs = require('fs')
+const yaml = require('js-yaml')
 
-
-const identity = 'Agency'
-//Paths in the code are relative to the folder of the fabric tutorial
-const walletPath = path.resolve(__dirname, `/home/winterschool20/Desktop/Winter_school/Fabric_school_material/test-network/${identity}/wallet`)
+const networkPath = path.resolve(__dirname, '../test-network')
+let connProfiles = {
+    'suppliera.quotation.com': 'connection-org1.yaml',
+    'supplierb.quotation.com': 'connection-org2.yaml',
+    'agency.quotation.com': 'connection-org3.yaml'
+}
 
 let finalConnection = {}
-let contract = {}
+
 class test {
 
     constructor() {}
-    //method for creating the identity related to an organization interacting with the network
-    static async createIdentity() {
+
+    // Method for creating the identity related to an organization interacting with the network
+    /**
+     * 
+     * @param {String} walletIdentity | wallet identity label 
+     * @param {String} organization | organization domain
+     * @param {String} orgMspId | organizazion MSP ID
+     */
+    static async createIdentity(walletIdentity, organization, orgMspId) {
         try {
-           
+            const walletPath = path.resolve(__dirname, `../test-network/${walletIdentity}/wallet`)
             //create new wallet
             const wallet = await Wallets.newFileSystemWallet(walletPath)
             //get identity from wallet
-            const userExists = await wallet.get(identity)
+            const userExists = await wallet.get(walletIdentity)
             if (userExists) {
                 console.log(`WARN: An identity for the client user "${identity}" already exists in the wallet`)
             }
             //get credentials (certificate and private key) from the peer organization folder
-            const credPath = '/home/winterschool20/Desktop/Winter_school/Fabric_school_material/test-network/organizations/peerOrganizations/agency.quotation.com/users/User1@agency.quotation.com'
-            const certificate = fs.readFileSync(path.join(credPath, '/msp/signcerts/User1@agency.quotation.com-cert.pem')).toString();
-            const privateKey = fs.readFileSync(path.join(credPath, '/msp/keystore/priv_sk')).toString();
+            organization = organization.toLowerCase()
+            const credPath = path.join(networkPath, `/organizations/peerOrganizations/${organization}/users/User1@${organization}`)
+            const certificate = fs.readFileSync(path.join(credPath, `/msp/signcerts/User1@${organization}-cert.pem`)).toString()
+            const privateKey = fs.readFileSync(path.join(credPath, '/msp/keystore/priv_sk')).toString()
             
-            const identityWallet = {
+            const identity = {
                 credentials: { certificate, privateKey },
-                mspId: 'AgencyMSP',
+                mspId: orgMspId,
                 type: 'X.509'
             }
-            console.log("first function ok")
 
-            await wallet.put(identity, identityWallet)
+            await wallet.put(walletIdentity, identity)
         } catch (err) {
             console.log(err)
         }
     }
     
-    //Method for creating the connection with the blockchain
-    static async createConnection(){
+    // Method for creating the connection with the blockchain
+    /**
+     * 
+     * @param {*} walletIdentity | wallet identity label 
+     * @param {*} organization | organization domain
+     */
+    static async createConnection(walletIdentity, organization) {
         try {
-           
-            const wallet = await Wallets.newFileSystemWallet(walletPath);
+            const walletPath = path.resolve(__dirname, `../test-network/${walletIdentity}/wallet`)
+            const wallet = await Wallets.newFileSystemWallet(walletPath)
+
             //get the connection profile file
-            const connOrg1Path = '/home/winterschool20/Desktop/Winter_school/Fabric_school_material/test-network/organizations/peerOrganizations/agency.quotation.com/connection-org3.yaml'
-            let connectionProfile = yaml.safeLoad(fs.readFileSync(connOrg1Path, 'utf8'));
+            const connFile = connProfiles[organization]
+            const connOrgPath = path.join(networkPath, `/organizations/peerOrganizations/${organization}/${connFile}`)
+            const connectionProfile = yaml.safeLoad(fs.readFileSync(connOrgPath, 'utf8'))
+
             //create connection option object
-            let connectionOptions = {
-                identity: identity,
+            const connectionOptions = {
+                identity: walletIdentity,
                 wallet: wallet,
                 discovery: { enabled: true, asLocalhost: true },
                 eventHandlerOptions: {
-                // if strategy set to null, it will not wait for any commit events to be received from peers
+                    // if strategy set to null, it will not wait for any commit events to be received from peers
                     strategy: DefaultEventHandlerStrategies.MSPID_SCOPE_ALLFORTX
                 }
-            };
-    
-            
-           // save connection profile and option in global object 
-           // global.ConnectionProfiles[connectionID] = { connectionProfile, connectionOptions };
-           finalConnection = { connectionProfile, connectionOptions }
-            console.log("tutto ok seconda funzione")
+            }
+
+            // save connection profile and option in global object 
+            finalConnection = { connectionProfile, connectionOptions }
 
         } catch (err) {
             console.log(err)
         }
     }
     
-    //method for sending a transaction 
-    static async submitT(){
-            try {
-            
-                //array containing the parameters to pass to the smart contract function
-                let transactionParams = ['quotation1']
-                //connect to the gateway to access the blockchain
-                const gateway = new Gateway();
+    // Method for sending a transaction
+    /**
+     * 
+     * @param {String} channel 
+     * @param {String} transactionName 
+     * @param {Array} transactionParams 
+     */
+    static async submitT(channel, transactionName, transactionParams) {
+        try {
+            // connect to the gateway to access the blockchain
+            const gateway = new Gateway()
+            await gateway.connect(finalConnection.connectionProfile, finalConnection.connectionOptions)
                 
-                //const conn = global.ConnectionProfiles[connectionID];
-                const conn = finalConnection
-                await gateway.connect(conn.connectionProfile, conn.connectionOptions);
-                
-                //get and connect to the channel where the caincode is deployed
-                const network = await gateway.getNetwork('quotationchannel1');
-                //get the smart contract from the network
-                contract = await network.getContract('quotation');
-                let resp = null
-                let listener = async (event) => {
+            // get and connect to the channel where the caincode is deployed
+            const network = await gateway.getNetwork(channel)
+            // get the smart contract from the network
+            const contract = await network.getContract('quotation')
+
+            const listener = async (event) => {
+                const asset = JSON.parse(event.payload.toString())
+			
+			    console.log(`-- Contract Event Received: ${event.eventName} - ${JSON.stringify(asset.data)}`)
+			
+			    console.log(`*** Event: ${event.eventName}:${asset.ID}`)
+			
+			    const eventTransaction = event.getTransactionEvent()
+			    console.log(`*** transaction: ${eventTransaction.transactionId} status:${eventTransaction.status}`)
 		
-			const asset = JSON.parse(event.payload.toString());
-			
-			console.log(`-- Contract Event Received: ${event.eventName} - ${JSON.stringify(asset.data)}`);
-			
-			console.log(`*** Event: ${event.eventName}:${asset.ID}`);
-			
-			const eventTransaction = event.getTransactionEvent();
-			console.log(`*** transaction: ${eventTransaction.transactionId} status:${eventTransaction.status}`);
-		
-			const eventBlock = eventTransaction.getBlockEvent();
-			console.log(`*** block: ${eventBlock.blockNumber.toString()}`);
-		};
-		// now start the client side event service and register the listener
-		console.log(`--> Start contract event stream to peer in Org1`);
-		await contract.addContractListener(listener);
-		//test query
-            	const resultBuffer = await contract.evaluateTransaction('getQuotation','quotation1');
-            	
-            	console.log(JSON.parse(resultBuffer.toString('utf8')));
-       	 //submit transaction
-                if(!transactionParams || transactionParams === '')
-                    resp = await contract.submitTransaction('getQuotation');
-                else
-                    resp = await contract.submitTransaction('getQuotation', transactionParams);
-                
-                gateway.disconnect();
-                console.log(resp.toString())
-                contract.removeContractListener(listener);
-        
-            } catch (err) {
-                console.log(err)
+			    const eventBlock = eventTransaction.getBlockEvent()
+			    console.log(`*** block: ${eventBlock.blockNumber.toString()}`)
             }
+
+		    // now start the client side event service and register the listener
+		    console.log(`--> Start contract event stream to peer in Org1`)
+            await contract.addContractListener(listener)
+
+		    // test query
+            // const resultBuffer = await contract.evaluateTransaction(transactionName,'quotation1')
+            // console.log(JSON.parse(resultBuffer.toString('utf8')))
+
+            // submit transaction
+            let resp = null
+            if(!transactionParams || transactionParams === '')
+                resp = await contract.submitTransaction(transactionName)
+            else
+                resp = await contract.submitTransaction(transactionName, transactionParams)
+                
+            gateway.disconnect()
+            console.log(resp.toString())
+            contract.removeContractListener(listener)
+        
+        } catch (err) {
+            console.log(err)
+        }
     }
 
 }
 
 const main = async () => {
     try {
-        await test.createIdentity()
-        await test.createConnection()
-        await test.submitT()
+        const identity = 'Agency'
+        const organization = 'agency.quotation.com'
+        const orgMspId = 'AgencyMSP'
+        const channel = 'quotationchannel1'
+        const transactionName = 'getQuotation'
+        const transactionParams = ['quotation1']  // array containing the parameters to pass to the smart contract function
+
+        await test.createIdentity(identity, organization, orgMspId)
+        await test.createConnection(identity, organization)
+        await test.submitT(channel, transactionName, transactionParams)
     } catch (error) {
         console.log(error)
     }
-  
-
-  //  await test.createConnection()
 }
 main()
 
