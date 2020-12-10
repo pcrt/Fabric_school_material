a174547d9d59ac312c674d30c6c5ba06e91252cd
AlessandroMarcellettiUnicam1 <alessand.marcelletti@unicam.it>
2020-12-10 15:24:54 +0100

    added general function invocation

--- test-network/scripts/query.sh
+++ test-network/scripts/query.sh
@@ -5,57 +5,22 @@ source scripts/utils.sh
 source scripts/envVar.sh
 export FABRIC_CFG_PATH=${PWD}/../config
 
-requestQuotationAgencyToSupplierA() {
-  # 1 = SupplierA, 2 = SupplierB, 3 = Agency
-  setGlobals 3
-  local ORDERER=localhost:7050
-  local PEER0_ORG1=localhost:7051   # SupplierA
-  local PEER0_ORG3=localhost:11051  # Agency
-  local NAME_CC=quotation
-  local CHANNEL="quotationchannel1"
-  local TX='{"Args":["requestQuotation","quotation2","jeans","5"]}'
-  
-  set -x
-  peer chaincode invoke -o $ORDERER --tls true --cafile $ORDERER_CA -C $CHANNEL -n $NAME_CC --peerAddresses $PEER0_ORG1 --tlsRootCertFiles $PEER0_ORG1_CA --peerAddresses $PEER0_ORG3 --tlsRootCertFiles $PEER0_ORG3_CA -c $TX
-  res=$?
-  { set +x; } 2>/dev/null
-  cat log.txt
-  verifyResult $res "invoke transaction 'requestQuotation' has failed"
-  successln "invoke transaction 'requestQuotation' success"
-}
 
-requestQuotation() {
-  # 1 = SupplierA, 2 = SupplierB, 3 = Agency
-  setGlobals 3
-
-  local ORDERER=localhost:7050
-  local PEER0_ORG1=localhost:7051   # SupplierA
-  local PEER0_ORG3=localhost:11051  # Agency
-  local NAME_CC=quotation
-  local CHANNEL="quotationchannel1"
-  local TX='{"Args":["requestQuotation","quotation2","jeans","25"]}'
-  
-  set -x
-  peer chaincode invoke -o $ORDERER --tls true --cafile $ORDERER_CA -C $CHANNEL -n $NAME_CC --peerAddresses $PEER0_ORG1 --tlsRootCertFiles $PEER0_ORG1_CA --peerAddresses $PEER0_ORG3 --tlsRootCertFiles $PEER0_ORG3_CA -c $TX
-  res=$?
-  { set +x; } 2>/dev/null
-  cat log.txt
-  verifyResult $res "invoke transaction 'requestQuotation' has failed"
-  successln "invoke transaction 'requestQuotation' success"
-}
-
-getQuotation() {
+invokeFunction() {
   # 1 = SupplierA, 2 = SupplierB, 3 = Agency
   IDENTITY=$1
   CHANNEL=$2
+  FUNCTION_NAME=$3
+  PARAMETERS=$4
   setGlobals ${IDENTITY}
   
   local ORDERER=localhost:7050
   local PEER0_ORG3=localhost:11051  # Agency
   local NAME_CC=quotation
   local CHANNEL="quotationchannel"${CHANNEL}
-  local TX='{"Args":["getQuotation","quotation1"]}'
-	set -x
+  #local TX='{"Args":["'${FUNCTION_NAME}'","'${PARAMETERS}'"]}'
+  local TX='{"Args":["'${FUNCTION_NAME}'","quotation1","50"]}'
+  	set -x
   
   case $1 in
   1)
@@ -68,8 +33,8 @@ getQuotation() {
    res=$?
   { set +x; } 2>/dev/null
   cat log.txt
-  verifyResult $res "invoke transaction 'getQuotations' has failed"
-  successln "invoke transaction 'getQuotation' success"
+  verifyResult $res "invoke transaction '${FUNCTION_NAME}' has failed"
+  successln "invoke transaction '${FUNCTION_NAME}' success"
 }
 
 provideQuotation() {
@@ -90,32 +55,11 @@ provideQuotation() {
   successln "invoke transaction 'provideQuotation' success"
 }
 
-acceptQuotation() {
-  # 1 = SupplierA, 2 = SupplierB, 3 = Agency
-  setGlobals 3
-
-  local ORDERER=localhost:7050
-  local PEER0_ORG1=localhost:7051   # SupplierA
-  local PEER0_ORG3=localhost:11051  # Agency
-  local NAME_CC=quotation
-  local CHANNEL="quotationchannel1"
-  local TX='{"Args":["acceptQuotation","quotation1","accepted"]}'
-	set -x
-  peer chaincode invoke -o $ORDERER --tls true --cafile $ORDERER_CA -C $CHANNEL -n $NAME_CC --peerAddresses $PEER0_ORG1 --tlsRootCertFiles $PEER0_ORG1_CA --peerAddresses $PEER0_ORG3 --tlsRootCertFiles $PEER0_ORG3_CA -c $TX
-  res=$?
-  { set +x; } 2>/dev/null
-  cat log.txt
-  verifyResult $res "invoke transaction 'accept' has failed"
-  successln "invoke transaction 'accept' success"
-}
 
 
+invokeFunction 1 1 provideQuotation
+invokeFunction 3 1 getQuotation "quotation1"
 
-#provideQuotation
-#acceptQuotation
-getQuotation 1 1
-#getQuotation 2 2
-#requestQuotation
 
 
 
